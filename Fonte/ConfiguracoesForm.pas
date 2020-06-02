fd38ea6860d21eccc7503b3d51cb9385b5a41e12
Paulo Henrique <paulohenrique7010@gmail.com>
2020-06-02 20:19:00 -0300

    Começando a ler excel Criação do datamodule para conexão

---
+++ Fonte/ConfiguracoesForm.pas
@@ -0,0 +1,24 @@
+unit ConfiguracoesForm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TConfiguracoesFrm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  ConfiguracoesFrm: TConfiguracoesFrm;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ Fonte/__history/ImportarExcelParaBanco.dpr.~1~
@@ -0,0 +1,14 @@
+program ImportarExcelParaBanco;
+
+uses
+  Vcl.Forms,
+  PrincipalForm in 'PrincipalForm.pas' {PrincipalFrm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TPrincipalFrm, PrincipalFrm);
+  Application.Run;
+end.
